018aa789ba1d715317a323d8ece83e7954d1ff66
Jigyasu97 <jigyasu.khandelwal@research.iiit.ac.in>
2023-06-21 12:45:12 +0530

    Main.py

---
+++ Exploratory Data Analysis/Black friday/Main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import logging as logger
+
+from Tester import *
+from Dataset import *
+from Missingval import *
+
+df_train = dataset()
+df = df_train.copy()
+
+# Dropping User ID column
+df.drop(['User_ID'],axis=1,inplace=True)
+
+# Categorical data to Numerical data
+df_cat = cat_2_num(df)
+
+# Handling missing values
+data_mean_fillna1 = missing_val(df_cat)
+logger.debug("Harmless debug Message")
